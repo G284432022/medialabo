01bc4c1b90c398efadc75edc4f35b8f8b78ceae7
G284432022 <G284432022@st.takushoku-u.ac.jp>
2023-06-09 15:45:51 +0900

    kazuate完成

--- kazuate.js
+++ kazuate.js
@@ -7,18 +7,21 @@ let kaisu = 0;
 let n = document.querySelector("span#kaisu");
 let a = document.querySelector("span#answer");
 let re = document.querySelector("p#result");
+//　回答ボタン
+let b = document.querySelector('#print');
+b.addEventListener('click',hantei);
+
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
-hantei();
-hantei();
-hantei();
+//hantei();
 //結果の検索
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 2;
+  //pnput
+  let i = document.querySelector('input[name="seisu"]');
+  let yoso = Number(i.value);
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
@@ -33,7 +36,7 @@ function hantei() {
   }else if(kotae < yoso){
     re.textContent='まちがい．答えはもっと小さいですよ';
   }
-  if(kaisu<=4){
+  if(kaisu>=4){
     re.textContent='答えは'+ kotae+ 'でした．すでにゲームは終わっています';
   }
 }
